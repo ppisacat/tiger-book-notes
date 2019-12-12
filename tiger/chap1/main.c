49ec44b058f61ba04ba5f444cc96184741fee3ae
pxl <pengxiaolong@jingyoutech.com>
2019-12-12 21:02:43 +0800

    upd

---
+++ tiger/chap1/main.c
@@ -0,0 +1,19 @@
+
+#include "prog1.h"
+#include <assert.h>
+#include <stdio.h>
+
+int maxargs(A_stm s)
+{
+  return 1;
+}
+
+void interp(A_stm s)
+{
+  return;
+}
+
+int main(int argc, char** argv)
+{
+  return 0;
+}
--- tiger/chap4/table.c
+++ tiger/chap4/table.c
@@ -3,36 +3,42 @@
  * Copyright (c) 1997 Andrew W. Appel.
  */
 
-#include <stdio.h>
-#include "util.h"
 #include "table.h"
+#include "util.h"
+#include <stdio.h>
 
 #define TABSIZE 127
 
 typedef struct binder_ *binder;
-// the key here is the identifier in the AbstractParseTree 
-// the value is the type of the identifier 
-struct binder_ {void *key; void *value; binder next; void *prevtop;};
+// the key here is the identifier in the AbstractParseTree
+// the value is the type of the identifier
+struct binder_ {
+  void *key;
+  void *value;
+  binder next;
+  void *prevtop;
+};
 struct TAB_table_ {
   binder table[TABSIZE];
-  void *top;  // what's this for?
+  void *top; // what's this for?
 };
 
-
-static binder Binder(void *key, void *value, binder next, void *prevtop)
-{binder b = checked_malloc(sizeof(*b));
- b->key = key; b->value=value; b->next=next; b->prevtop = prevtop; 
- return b;
+static binder Binder(void *key, void *value, binder next, void *prevtop) {
+  binder b = checked_malloc(sizeof(*b));
+  b->key = key;
+  b->value = value;
+  b->next = next;
+  b->prevtop = prevtop;
+  return b;
 }
 
-TAB_table TAB_empty(void)
-{ 
- TAB_table t = checked_malloc(sizeof(*t));
- int i;
- t->top = NULL;
- for (i = 0; i < TABSIZE; i++)
-   t->table[i] = NULL;
- return t;
+TAB_table TAB_empty(void) {
+  TAB_table t = checked_malloc(sizeof(*t));
+  int i;
+  t->top = NULL;
+  for (i = 0; i < TABSIZE; i++)
+    t->table[i] = NULL;
+  return t;
 }
 
 /* The cast from pointer to integer in the expression
@@ -44,34 +50,37 @@ TAB_table TAB_empty(void)
  * reasonable and repeatable index into the table will result.
  */
 
-void TAB_enter(TAB_table t, void *key, void *value)
-{int index;
- assert(t && key);
- index = ((unsigned)key) % TABSIZE;
- t->table[index] = Binder(key, value,t->table[index], t->top);
- t->top = key;
+void TAB_enter(TAB_table t, void *key, void *value) {
+  int index;
+  assert(t && key);
+  index = ((unsigned)key) % TABSIZE;
+  t->table[index] = Binder(key, value, t->table[index], t->top);
+  t->top = key;
 }
 
-void *TAB_look(TAB_table t, void *key)
-{int index;
- binder b;
- assert(t && key);
- index=((unsigned)key) % TABSIZE;
- for(b=t->table[index]; b; b=b->next)
-   if (b->key==key) return b->value;
- return NULL;
+void *TAB_look(TAB_table t, void *key) {
+  int index;
+  binder b;
+  assert(t && key);
+  index = ((unsigned)key) % TABSIZE;
+  for (b = t->table[index]; b; b = b->next)
+    if (b->key == key)
+      return b->value;
+  return NULL;
 }
 
 void *TAB_pop(TAB_table t) {
-  void *k; binder b; int index;
-  assert (t);
+  void *k;
+  binder b;
+  int index;
+  assert(t);
   k = t->top;
-  assert (k);
+  assert(k);
   index = ((unsigned)k) % TABSIZE;
   b = t->table[index];
   assert(b);
   t->table[index] = b->next;
-  t->top=b->prevtop;
+  t->top = b->prevtop;
   return b->key;
 }
 
@@ -79,12 +88,13 @@ void TAB_dump(TAB_table t, void (*show)(void *key, void *value)) {
   void *k = t->top;
   int index = ((unsigned)k) % TABSIZE;
   binder b = t->table[index];
-  if (b==NULL) return;
-  t->table[index]=b->next;
-  t->top=b->prevtop;
-  show(b->key,b->value);
-  TAB_dump(t,show);
-  assert(t->top == b->prevtop && t->table[index]==b->next);
-  t->top=k;
-  t->table[index]=b;
+  if (b == NULL)
+    return;
+  t->table[index] = b->next;
+  t->top = b->prevtop;
+  show(b->key, b->value);
+  TAB_dump(t, show);
+  assert(t->top == b->prevtop && t->table[index] == b->next);
+  t->top = k;
+  t->table[index] = b;
 }
